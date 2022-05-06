06b0e059d4acb884121015a10bcb43bd7b86ae41
diannaqui <103956753+diannaqui@users.noreply.github.com>
2022-05-06 00:44:54 -0600

    Update card.py
    Generated the random numbers

--- card.py
+++ card.py
@@ -1,7 +1,14 @@
+import random
 
 
+class Display_card:
+    """Generates a random card between 1 to 13
 
-    def display_card(self):
-        """Generates a random card between 1 to 13
-        
-        """
+    """
+
+    def __init__(self):
+        self.card_number = 0
+
+    def draw(self):
+        self.card_number = random.randint(1, 13)
+        return self.card_number
