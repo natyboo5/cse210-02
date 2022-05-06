bae756caa453d7b224156aa895dfda90d9d7bf9d
diannaqui <103956753+diannaqui@users.noreply.github.com>
2022-05-06 00:49:54 -0600

    Update dealer.py
    Variables and functions

--- dealer.py
+++ dealer.py
@@ -1,5 +1,7 @@
 """ Hilo Game """
 
+from card import Display_card
+
 class Dealer:
     """A person who directs the game. 
     
@@ -18,7 +20,12 @@ class Dealer:
         Args:
             self (Dealer): an instance of Dealer.
         """
-
+        self.is_playing = ''
+        self.card = 0
+        self.guess_card = 0
+        self.guess = ''
+        self.points = 0
+        self.new_points = 0
 
 
     def start_game(self):
@@ -27,13 +34,48 @@ class Dealer:
         Args:
             self (Dealer): an instance of Dealer.
         """
+        self.points = 300
+        self.is_playing = True
+
+        while self.is_playing:
+            print()
+            display_card = Display_card()
+            self.card = display_card.draw()
+            print(f'The card is: {self.card}')
+
+            self.get_input()
+            self.output()
+            self.update_points()
+            self.play_again()
 
     def get_input(self): 
+        
+        self.guess = input('Higher or Lower [h/l]: ')
 
+        display_card = Display_card()
+        self.guess_card = display_card.draw()
 
+        print(f'Next card was: {self.guess_card}')
+
+        
     def update_points(self):
-
+        
+        self.points += self.new_points
+        print(f'Your score is: {self.points}')
+        
     
     def output(self):
         
+        if (self.card < self.guess_card and self.guess == 'l') or (self.card > self.guess_card and self.guess == 'h'):
+            self.new_points = -75
+        else:
+            self.new_points = 100
     
+    
+    def play_again(self):
+        play = input('Play again [y/n]: ')
+
+        if play == 'y':
+            self.is_playing = True
+        else:
+            self.is_playing = False
