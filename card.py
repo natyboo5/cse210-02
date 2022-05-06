d9886b0d04bcbb81f93c37d4bc69feb981e442b0
Natalia <nparedes517@yahoo.com>
2022-05-06 00:48:01 -0400

    adding initial files

---
+++ card.py
@@ -0,0 +1,7 @@
+
+
+
+    def display_card(self):
+        """Generates a random card between 1 to 13
+        
+        """
---
+++ dealer.py
@@ -0,0 +1,38 @@
+
+class Dealer:
+    """A person who directs the game. 
+    
+    The dealer's responsability is to control the sequence of the game
+    
+    Attributes: 
+        card(int): The value of the card drawn
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.
+    """
+
+    def _init_(self):
+        """Constructor a new Dealer.
+        
+        Args:
+            self (Dealer): an instance of Dealer.
+        """
+
+
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Dealer): an instance of Dealer.
+        """
+
+    def get_input(self): 
+
+
+    def update_points(self):
+
+    
+    def output(self):
+        
+    
