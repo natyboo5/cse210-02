5cb7a047e9c2ec608836fe2c408b998d3aae5797
Natalia <nparedes517@yahoo.com>
2022-05-07 19:20:15 -0400

    updated code

--- card.py
+++ card.py
@@ -1,7 +1,16 @@
+import random
 
 
+class Display_card:
+    """Generates a random card between 1 to 13
+    """
 
-    def display_card(self):
-        """Generates a random card between 1 to 13
+    def __init__(self):
+        self.card_number = 0
+
+    def drawn(self):
+        """Draws a new card between 1 and 13, and return the number of the card. 
         
         """
+        self.card_number = random.randint(1, 13)
+        return self.card_number
--- dealer.py
+++ dealer.py
@@ -1,3 +1,6 @@
+""" Hilo Game """
+
+from card import Display_card
 
 class Dealer:
     """A person who directs the game. 
@@ -17,7 +20,12 @@ class Dealer:
         Args:
             self (Dealer): an instance of Dealer.
         """
-
+        self.is_playing = ''
+        self.card = 0
+        self.guess_card = 0
+        self.guess = ''
+        self.points = 0
+        self.new_points = 0
 
 
     def start_game(self):
@@ -26,13 +34,92 @@ class Dealer:
         Args:
             self (Dealer): an instance of Dealer.
         """
+        self.points = 300
+        self.is_playing = True
+
+        while self.is_playing:
+            print()
+            display_card = Display_card()
+            self.card = display_card.drawn()
+            print(f'The card is: {self.card}')
+
+            self.get_input()
+            self.output()
+            self.update_points()
+            
+            if self.is_playing == False:
+                break
+                
+            self.play_again()
 
     def get_input(self): 
+        """Ask the user to guess if the next card is higher or lower, and confirm the input is entered correctly (either "h" or "l" only).
+        After the input is validated, call the function draw(self) and display a new card.
 
+        Args:
+            self (Dealer): an instance of Dealer.
+        """
+        
+        while True:
+            self.guess = input('Higher or Lower [h/l]: ').lower()
 
+            if self.guess == 'l' or self.guess == 'h':
+
+                while True:
+                    display_card = Display_card()
+                    self.guess_card = display_card.drawn()
+                    if self.guess_card == self.card:
+                        continue
+                    else:
+                        break
+
+                print(f'Next card was: {self.guess_card}')
+                break
+                
+            else:
+                print('Wrong letter')
+                continue
+
+        
     def update_points(self):
-
-    
-    def output(self):
+        """A method that determines the score for the entire game
+        
+        Args:
+            self (Dealer): an instance of Dealer.
+        """
+        
+        self.points += self.new_points
+        if self.points <= 0:
+            self.is_playing = False
+            print('You do not have enough points')
+        else:
+            print(f'Your score is: {self.points}')
         
     
+    def output(self):
+    
+        """A method that determines the score for one round of play
+        
+        Args:
+            self (Dealer): an instance of Dealer.
+        """
+        
+        if (self.card < self.guess_card and self.guess == 'l') or (self.card > self.guess_card and self.guess == 'h'):
+            self.new_points = -75
+        else:
+            self.new_points = 100
+    
+    
+    def play_again(self):
+        """Ask the user if he wants to play another round.
+        
+        Args:
+            self (Dealer): an instance of Dealer.
+        """
+
+        play = input('Play again [y/n]: ')
+
+        if play == 'y':
+            self.is_playing = True
+        else:
+            self.is_playing = False
